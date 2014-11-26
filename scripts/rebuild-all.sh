985c4369998f4788bf5dca1f26a142cb177992b8
Sebastian Lauwers <sebastian.lauwers@interact-iv.com>
2014-11-26 16:28:12 +0100

    Apparently I've forgotten how to loop through a directory

--- scripts/rebuild-all.sh
+++ scripts/rebuild-all.sh
@@ -2,6 +2,6 @@
 
 # Please call this from the repository's root directory.
 
-for dir in images; do
+for dir in images/*; do
 	docker build -t "slauwers/centos:$dir" "$dir"
 done
--- scripts/update-base-images.sh
+++ scripts/update-base-images.sh
@@ -2,6 +2,6 @@
 
 # Please call this from the repository's root directory.
 
-for dir in images; do
+for dir in images/*; do
 	docker pull $(awk '/^FROM/ { print $$2; exit }' "$dir/Dockerfile")
 done
