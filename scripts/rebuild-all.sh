f1db943db04af3945d00b01ebca578aa582ebcda
Sebastian Lauwers <sebastian.lauwers@interact-iv.com>
2014-11-26 16:34:14 +0100

    Refactored rebuild-all

--- scripts/rebuild-all.sh
+++ scripts/rebuild-all.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 # Please call this from the repository's root directory.
+repository="slauwers/centos"
 
 for dir in images/*; do
-	docker build -t "slauwers/centos:$dir" "$dir"
+	tag=$(basename "$dir")
+	docker build -t "$repository:$tag" "$dir"
 done
